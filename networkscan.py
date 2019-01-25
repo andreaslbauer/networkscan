5df3e74ff78c0185b00a244ecd8ea45a2ce800a4
Andreas L Bauer <andreaslbauer@gmail.com>
2019-01-24 19:41:49 -0500

    Write discovered data to CouchDB instance

--- networkscan.py
+++ networkscan.py
@@ -10,15 +10,26 @@ import os
 import paramiko
 import time
 import socket
+import json
 from requests import get
+import datetime
 
 # logging facility: https://realpython.com/python-logging/
 import logging
+
+# for couch db
+import pycouchdb
 
 # port list and base address to scan
 portList = ["5000", "8080", "8081", "8088", "80", "8089", "8100"]
 baseAddress = "192.168.0."
 interactive = None
+
+# our couch DB access
+couchDBServerUrl = "http://192.168.0.150:5984/"
+couchDBServer = None
+couchDB = None
+
 
 # print but only if interactive flag is "on"
 def printIfInteractive (*args, **kwargs):
@@ -31,93 +42,162 @@ def createAddressList() :
     for i in range(1, 255):
         addresses.append(baseAddress + str(i))
 
-    #addresses = ["192.168.0.100", "192.168.0.133"]
+    #addresses = ["192.168.0.100", "192.168.0.133", "192.168.0.1", "192.168.0.150", "127.0.0.1"]
 
     return addresses
 
-# attempt to ping an address
-def testPing(address):
-    printIfInteractive(f"\rPinging {address}                                             ", end="")
+# clsas networkProber to check network properties for an address and record findings in class data members
+class networkProber :
 
-    # first try to ping the address
-    pingResponse = os.system("ping -c 2 -w 3 " + address + " > /dev/null 2>&1")
-    if pingResponse == 0:
-        hostname = "unknown"
-        try:
-            hostname = socket.gethostbyaddr(address)[0]
-        except Exception as e:
-            pass
+    # constructor; it initializes all data members per passed parameters
+    def __init__ (self, ipAddress):
+        self.ipAddress = ipAddress
+        self._id = ipAddress
+        self.pingResponseString = ""
+        self.hostname = ""
+        self.hostFound = None
 
-            printIfInteractive(f"\rPing response from {address} hostname {hostname}: {pingResponse}")
+    # attempt to ping an address
+    def testPing(self):
+        address = self.ipAddress
+        printIfInteractive(f"\rPinging {address}                                             ", end="")
 
-# attempt ssh connection
-def testSSH(address):
-    printIfInteractive(f"\rTesting ssh for {address}                  ", end="")
-
-    nbytes = 4096
-    command = "hostname && uname -a && uptime && df -h . && ps -elf | grep python | grep -v grep"
-    username = "pi"
-    password = "alex5"
-    hostname = address
-
-    try:
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname=hostname, username=username, password=password)
-        stdin, stdout, stderr = client.exec_command(command)
-
-        output = stdout.readlines()
-        errors = stderr.readlines()
-
-        # create single string from output
-        outputStr = ""
-        for line in output:
-            outputStr = outputStr + line
-
-        # create single string from errors
-        errorStr = ""
-        for line in errors:
-            errorStr = errorStr + line
-
-            printIfInteractive(f"\rssh response at {address}: \n{outputStr} \n{errorStr}")
-
-    except Exception as e:
-        pass
-
-    try:
-        client.close()
-
-    except Exception as e:
-        pass
-
-
-# test address for HTTP responses
-def testHTTP(address):
-
-    success = False
-
-    # now look for HTTP responses
-    for port in portList:
-
-        # build an URL string
-        success = False
-
-        url = f"http://{address}:{port}"
-        try:
-            printIfInteractive(f"\rTesting HTTP get for {url}              ", end="")
-            response = requests.get(url, timeout=1)
+        # first try to ping the address
+        pingResponse = os.system("ping -c 2 -w 3 " + address + " > /dev/null 2>&1")
+        self.pingResponseString = pingResponse
+        if pingResponse == 0:
             hostname = "unknown"
             try:
                 hostname = socket.gethostbyaddr(address)[0]
+                self.hostname = hostname
+
             except Exception as e:
                 pass
 
-                printIfInteractive(f"\rReceived response at {url}: {response.status_code} hostname: {hostname}")
-            success = True
+            finally:
+                printIfInteractive(f"\rPing response from {address} hostname {hostname}: {pingResponse}")
+                #logging.info("Hostname is %s", hostname)
+                logging.info(f"Ping response from {address} hostname {hostname}: {pingResponse}")
+                self.hostFound = 1
+
+    def sendCmdssh(self, ipAddress, username, password, cmd):
+
+        sshResponse = ""
+
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(hostname=ipAddress, username=username, password=password)
+            stdin, stdout, stderr = client.exec_command(cmd)
+
+            output = stdout.readlines()
+            errors = stderr.readlines()
+
+            # create single string from output
+            outputStr = ""
+            for line in output:
+                outputStr = outputStr + line
+
+            # create single string from errors
+            errorStr = ""
+            for line in errors:
+                errorStr = errorStr + line
+
+            printIfInteractive(f"\rssh response at {ipAddress} for command {cmd}: \n{outputStr} \n{cmd}")
+            sshResponse = outputStr
+
         except Exception as e:
             pass
 
-    return success
+        try:
+            client.close()
+
+        except Exception as e:
+            pass
+
+        return sshResponse
+
+
+    # attempt ssh connection
+    def testSSH(self):
+        address = self.ipAddress
+        printIfInteractive(f"\rTesting ssh for {address}                  ", end="")
+
+        username = "pi"
+        password = "alex5"
+
+        self.sshResponse_uname = self.sendCmdssh(address, username, password, "uname -a")
+        self.sshResponse_uptime = self.sendCmdssh(address, username, password, "uptime")
+        self.sshResponse_ps_docker = self.sendCmdssh(address, username, password, "ps -elf | grep docker | grep -v grep")
+        self.sshResponse_ps_motion = self.sendCmdssh(address, username, password, "ps -elf | grep motion | grep -v grep")
+        self.sshResponse_docker_ps = self.sendCmdssh(address, username, password, "docker ps")
+        self.sshResponse_df = self.sendCmdssh(address, username, password, "df -h")
+        self.memTotal = self.sendCmdssh(address, username, password, "cat /proc/meminfo | grep MemTotal")
+
+
+    # test address for HTTP responses
+    def testHTTP(self):
+        address = self.ipAddress
+
+        success = False
+        ports = []
+
+        # now look for HTTP responses
+        for port in portList:
+
+            # build an URL string
+            success = False
+
+            url = f"http://{address}:{port}"
+            try:
+                printIfInteractive(f"\rTesting HTTP get for {url}              ", end="")
+                response = requests.get(url, timeout=1)
+                hostname = "unknown"
+                try:
+                    hostname = socket.gethostbyaddr(address)[0]
+                except Exception as e:
+                    pass
+
+                printIfInteractive(f"\rReceived response at {url}: {response.status_code} hostname: {hostname}")
+                success = True
+                self.hostFound = 1
+                ports.append(str(port))
+            except Exception as e:
+                pass
+
+        self.httpPorts = ports
+        return success
+
+    def probe(self):
+        self.testPing()
+        self.testSSH()
+        self.testHTTP()
+
+        now = datetime.datetime.now()
+        nowDateTime = str(now)
+        self.lastUpdated = nowDateTime
+        self.firstSeen = nowDateTime
+
+        if self.hostFound :
+            # if couch DB instance is initialized save the document
+            if (couchDB != None) :
+                try:
+                    doc = None
+                    try:
+                        doc = couchDB.get(self.ipAddress)
+                        self._rev = doc["_rev"]
+                        self.firstSeen = doc['firstSeen']
+                    except:
+                        pass
+
+                except pycouchdb.exceptions.Conflict as e:
+                    pass
+
+                doc = couchDB.save(self.__dict__)
+                logging.info("Stored record %s", doc)
+
+    def toJSON(self):
+        return self.__dict__
 
 
 # main program function
@@ -127,6 +207,14 @@ def main() :
     logging.info("***************************************************************")
     logging.info("networkscan Data Collector has started")
     logging.info("Working directory is %s", os.getcwd())
+
+    couchDBServer = pycouchdb.Server(couchDBServerUrl)
+    logging.info("CouchDB Server is %s", couchDBServerUrl)
+    logging.info("CouchDB Server version is %s", couchDBServer.info()['version'])
+
+    # initialize global variable couch DB
+    global couchDB
+    couchDB = couchDBServer.database("networkscan")
 
     try:
         hostname = socket.gethostname()
@@ -146,11 +234,10 @@ def main() :
     # create an address list and then iterate through each address and probe for network services
     addresses = createAddressList()
     for address in addresses:
-        testPing(address)
-        testSSH(address)
-        testHTTP(address)
+        prober = networkProber(address)
+        prober.probe()
 
-        printIfInteractive("Scan complete!                                          ")
+    printIfInteractive("Scan complete!                                          ")
 
 
 
@@ -160,11 +247,11 @@ logging.basicConfig(filename="/var/log/networkscan/networkscan.log", format='%(a
 
 if __name__ == '__main__':
 
-    try:
-        main()
+    #try:
+    main()
 
-    except Exception as e:
-        logging.exception("Exception occurred in main")
+    #except Exception as e:
+        #logging.exception("Exception occurred in main")
 
     logging.info("networkscan Data Collector has terminated")
 
